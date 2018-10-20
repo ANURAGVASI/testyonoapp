0628f6878f14dda79e49bd14ef44fecf2120a2d1
anurag.v70 <anurag.v70@wipro.com>
2018-10-21 01:04:40 +0530

    test

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const clickedUsers = require("./uniqueID");
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+mongoose.connect("mongodb://anuragvasi:anurag61@ds237373.mlab.com:37373/sbiintrestedusers",{ useNewUrlParser: true }, 
+    (err,data) => {
+    if(err){
+        console.log("error connecting mongoDB", err);
+    }
+    else{
+        console.log("sucessfully connecte dto DB");
+    }
+})
+
+app.get("/", (req,res) => {
+    res.write("redirecting..");
+    res.redirect("https://www.onlinesbi.com/");
+})
+
+app.get("/:customerid", (req,res,next) => {
+    const customerid = req.params.customerid;
+    console.log("",customerid);
+    const user = new clickedUsers({
+        clickedID:customerid
+    });
+
+    user.save((err,data) => {
+        if(err){
+            console.log('error saving to db', customerid);
+        }
+        if(data){
+            console.log('saved to DB');
+        }
+        res.write("redirecting..");
+        res.redirect("https://www.onlinesbi.com/");
+    });
+
+    
+});
+
+
+app.listen(3000, () => {
+    console.log("server running at port 3000");
+})
